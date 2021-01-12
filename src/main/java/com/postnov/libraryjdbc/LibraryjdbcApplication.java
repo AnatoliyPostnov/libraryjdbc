ef2c36e90f524fdc4805b2050d8433fdaccff12c
Anatoliy Postnov <postnov-90@mail.ru>
2021-01-12 16:19:44 +0300

    Убрал лишний bean modelMapper

--- src/main/java/com/postnov/libraryjdbc/LibraryjdbcApplication.java
+++ src/main/java/com/postnov/libraryjdbc/LibraryjdbcApplication.java
@@ -1,20 +1,13 @@
 package com.postnov.libraryjdbc;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryjdbcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryjdbcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryjdbcApplication.class, args);
+    }
 
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
-	
 }
